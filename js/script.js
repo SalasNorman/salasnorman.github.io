1822d3717b411798ca0df7474ec1fd147db2ab3e
SalasNorman <salasnorman015@gmail.com>
2026-07-01 08:42:25 +0800

    feat: add Odin Project card with expandable live demo list

--- js/script.js
+++ js/script.js
@@ -33,4 +33,15 @@
       header.classList.toggle('nav-open');
     });
   }
+
+  const demoBtn = document.querySelector('.project__demo-btn');
+  const demoList = document.querySelector('.project__demo-list');
+  const chevron = document.querySelector('.project__chevron');
+
+  if (demoBtn && demoList) {
+    demoBtn.addEventListener('click', function () {
+      demoList.classList.toggle('open');
+      if (chevron) chevron.classList.toggle('open');
+    });
+  }
 })();
